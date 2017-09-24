613a49f2942edce89c0ca66e35b96c6351d2f9ed
Brennan Vincello <brennanv03@gmail.com>
2017-09-24 09:59:50 -0700

    add boom

--- app.py
+++ app.py
@@ -9,16 +9,20 @@ app.secret_key = uuid.uuid4().hex
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    transcript = session.get('transcript', 'hello')
-    passage = session.get('passage', 'hello world')
+    transcript = session.get('transcript', '')
+    passage = session.get('passage', '')
     result = session.get('result', '')
-    if request.form:
+    if request.method == 'POST':
+    # if request.form:
         transcript = request.form['transcript']
         passage = request.form['passage']
         result = score(transcript, passage)
         result = str(result)[:6]
-    session['transcript'], session['passage'] = transcript, passage
-    session['result'] = result
+        session['transcript'], session['passage'] = transcript, passage
+        session['result'] = result
+        return render_template('boom.html', transcript=transcript,
+                               passage=passage, result=result)
+    print(transcript, passage, result)
     return render_template('index.html', transcript=transcript,
                            passage=passage, result=result)
 
