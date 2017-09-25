da8f02f97039fc4b11ac7f9772e17bebcee077a3
Brennan Vincello <brennanv03@gmail.com>
2017-09-24 22:45:34 -0700

    refactor mapping

--- tyto/score.py
+++ tyto/score.py
@@ -4,51 +4,60 @@
 #            'then the quick brown DOG jumped over the lazy fox')
 
 
+def listify(string):
+    # TODO regex to remove non-alpanumerics, strip
+    return string.lower().replace('  ', ' ').split()
+
+
+def inspect_slice(transcript, passage, i, step):
+    """ Inspect a passage slice and generate a mapping of transcript words
+    found in the slice as well as a negative index for words not found. """
+    hits, map_ = [], []
+    reverse_index = 0
+    chunk = passage[i:i + step]
+    for word in transcript:
+        if word in chunk[len(hits):]:  # Re-slice chunk to accomadate words already matched
+            index = chunk[len(hits):].index(word) + i + len(hits)
+            if not hits or index > max(hits):  # Only match words presented in the right order
+                hits.append(index)
+                map_.append(index)
+        else:
+            reverse_index -= 1
+            map_.append(reverse_index)  # Track extra words in transcript with a negative index
+        hit_count = len(hits)
+        fit = hit_count * (hit_count/len(chunk))  # Ratio of matches over chunk size, times number of matches
+    return map_, hits, fit
+
+
 def map_and_score(transcript, passage):
     """ Given two strings, transcript and passage, split them into a list and
     interate over the passage while inspecting a slice of increasing size.
     Determine the score using best fit and record the mappings for testing. """
     maps = []
     fit_max, hit_count_max = 0, 0
-    transcript = transcript.lower().replace('  ', ' ').split()
-    passage = passage.lower().replace('  ', ' ').split()
+    transcript, passage = listify(transcript), listify(passage)
     passage_length = len(passage)
     if not passage:
         return -1, []  # TODO raise BookPassageValueError
-    # if not list(set(transcript) & set(passage)):  # Stop searching when there's no common words
-    #     return 0, [list(range(-1, -1 * passage_length - 1, -1))]
     if transcript == passage:
         return 1, [list(range(passage_length))]
     for i in range(passage_length):
         step = 0
-        # commance inspecting chunks while chunks are the appropriate size
+        # Commance inspecting chunks while chunks are the appropriate size
         while i + step < passage_length and hit_count_max < passage_length - i:
             step += 1
-            if step >= hit_count_max:  # ensure we're only inpecting chunks >= max observed
-                chunk = passage[i:i + step]
-                hits, map_ = [], []
-                insert_index = 0
-                for word in transcript:
-                    if word in chunk[len(hits):]:  # re-slice to fix issue repeated words
-                        index = chunk[len(hits):].index(word) + i + len(hits)
-                        if not hits or index > max(hits):  # ensures expected word order
-                            hits.append(index)
-                            map_.append(index)
-                    else:
-                        insert_index -= 1
-                        map_.append(insert_index)  # placeholder for unexpected insertions
-                hit_count = len(hits)
-                fit = hit_count * (hit_count/len(chunk))  # best fit ratio times number of matches
-                if fit == fit_max:  # sometimes we'll find mappings with identical fit
+            if step >= hit_count_max:  # Skip slices smaller than max observed
+                map_, hits, fit = inspect_slice(transcript, passage, i, step)
+                if fit == fit_max:
                     maps.append(map_)
-                if fit > fit_max:  # keep track of the best observation
+                if fit > fit_max:
                     fit_max = fit
-                    hit_count_max = hit_count
+                    hit_count_max = len(hits)
                     maps = [map_]
-                if len(hits) == len(transcript):  # max score reached, skip to the next sliding chunk
+                if len(hits) == len(transcript):
                     break
             # Stop searching when there's no common words
-            if i == 0 and maps and max(maps[0]) < 0 and len(maps[0]) == passage_length:
+            if i == 0 and max(maps[0]) < 0 and len(maps[0]) == passage_length:
                 return 0, maps
     score = hit_count_max/passage_length
     return score, maps
