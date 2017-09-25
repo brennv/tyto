988227336385dd698bedd0b81bfdfebe690f2186
Brennan Vincello <brennanv03@gmail.com>
2017-09-24 20:45:37 -0700

    fix issue
    with repeated words

--- tyto/score.py
+++ tyto/score.py
@@ -1,3 +1,4 @@
+# Example transcript and passage
 # transcript = 'jumped over the lazy'
 # passage = ('the quick brown FOX jumped over the lazy dog '
 #            'then the quick brown DOG jumped over the lazy fox')
@@ -7,11 +8,15 @@ def map_and_score(transcript, passage):
     """ Given two strings, transcript and passage, split them into a list and
     interate over the passage while inspecting a slice of increasing size.
     Determine the score using best fit and record the mappings for testing. """
-    maps = []  # for debugging
+    maps = []
     fit_max, hit_count_max = 0, 0
     transcript = transcript.lower().replace('  ', ' ').split()
     passage = passage.lower().replace('  ', ' ').split()
     passage_length = len(passage)
+    if not passage:
+        return -1, []  # TODO raise BookPassageValueError
+    # if not list(set(transcript) & set(passage)):  # Stop searching when there's no common words
+    #     return 0, [list(range(-1, -1 * passage_length - 1, -1))]
     if transcript == passage:
         return 1, [list(range(passage_length))]
     for i in range(passage_length):
@@ -22,14 +27,16 @@ def map_and_score(transcript, passage):
             if step >= hit_count_max:  # ensure we're only inpecting chunks >= max observed
                 chunk = passage[i:i + step]
                 hits, map_ = [], []
+                insert_index = 0
                 for word in transcript:
-                    if word in chunk:
-                        index = chunk.index(word) + i
+                    if word in chunk[len(hits):]:  # re-slice to fix issue repeated words
+                        index = chunk[len(hits):].index(word) + i + len(hits)
                         if not hits or index > max(hits):  # ensures expected word order
                             hits.append(index)
                             map_.append(index)
                     else:
-                        map_.append(-1)  # placeholder for unexpected insertions
+                        insert_index -= 1
+                        map_.append(insert_index)  # placeholder for unexpected insertions
                 hit_count = len(hits)
                 fit = hit_count * (hit_count/len(chunk))  # best fit ratio times number of matches
                 if fit == fit_max:  # sometimes we'll find mappings with identical fit
@@ -40,37 +47,8 @@ def map_and_score(transcript, passage):
                     maps = [map_]
                 if len(hits) == len(transcript):  # max score reached, skip to the next sliding chunk
                     break
+            # Stop searching when there's no common words
+            if i == 0 and maps and max(maps[0]) < 0 and len(maps[0]) == passage_length:
+                return 0, maps
     score = hit_count_max/passage_length
     return score, maps
-
-
-def score(transcript, passage):
-    """ Just like map_and_score but without explicit mapping to save time. """
-    fit_max, hit_count_max = 0, 0
-    transcript = transcript.lower().replace('  ', ' ').split()
-    passage = passage.lower().replace('  ', ' ').split()
-    passage_length = len(passage)
-    if not passage:
-        return -1  # TODO raise PassageValueError
-    if transcript == passage:
-        return 1
-    for i in range(passage_length):
-        step = 0
-        while i + step < passage_length and hit_count_max < passage_length - i:
-            step += 1
-            if step >= hit_count_max:
-                chunk = passage[i:i + step]
-                hits = []
-                for word in transcript:
-                    if word in chunk:
-                        index = chunk.index(word) + i
-                        if not hits or index > max(hits):
-                            hits.append(index)
-                hit_count = len(hits)
-                fit = hit_count * (hit_count/len(chunk))
-                if fit > fit_max:
-                    fit_max = fit
-                    hit_count_max = hit_count
-                if len(hits) == len(transcript):
-                    break
-    return hit_count_max/passage_length
